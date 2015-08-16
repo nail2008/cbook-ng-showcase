4afeaa3c37064194538ab96cc4374421cdd9ccdd
nail2008 <duanyiding@gmail.com>
2015-08-16 23:24:04 +0800

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cbookNgShowcaseApp
+ * @description
+ * # cbookNgShowcaseApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cbookNgShowcaseApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cbookNgShowcaseApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cbookNgShowcaseApp
+ */
+angular.module('cbookNgShowcaseApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
